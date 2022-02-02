cce5792de9239737d576f43307169ce696d85da2
Etienne Dublé <etienne.duble@imag.fr>
2022-02-02 08:21:06 +0100

    rpi dockerfile: upgrade builder to bullseye

--- rpi/populate_rootfs.sh
+++ rpi/populate_rootfs.sh
@@ -7,25 +7,29 @@ os_type="$1"
 os_version="$2"
 
 populate_rootfs_debian() {
-    cd /rpi_fs; tar cfz ../rpi_fs_orig.tar.gz .; cd ..; rm -rf /rpi_fs
-    
     # populate target os filesystem
-    debootstrap --no-check-gpg --arch=armhf --foreign --variant=minbase \
+    debootstrap --no-merged-usr --no-check-gpg --arch=armhf --foreign \
+        --variant=minbase \
         --include raspbian-archive-keyring,apt-utils \
         $os_version "/rpi_fs" $RPI_DEBIAN_MIRROR_URL
+    # ensure /proc is a directory, not a symlink to the host /proc...
+    [ -L /rpi_fs/proc ] && rm /rpi_fs/proc && mkdir /rpi_fs/proc
+
+    # set an hostname
     echo "rpi-debian" > /rpi_fs/etc/hostname
+
+    # prepare apt repository definitions
     echo deb $RPI_DEBIAN_MIRROR_URL $os_version $RPI_DEBIAN_SECTIONS \
             > "/rpi_fs/etc/apt/sources.list.saved" && \
         echo deb http://archive.raspberrypi.org/debian/ $os_version main \
             >> "/rpi_fs/etc/apt/sources.list.saved"
-
-    cd /rpi_fs; tar xfz ../rpi_fs_orig.tar.gz; rm ../rpi_fs_orig.tar.gz 
 }
 
 case "$os_type" in
     "alpine")
-        # nothing to do, we will init the fs from
+        # not much to do, we will later init the fs from
         # official alpine image
+        mkdir /rpi_fs
         ;;
     "debian")
         # we initiate the fs using debootstrap
